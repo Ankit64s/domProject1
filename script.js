dd98166ba51ac30bf38cb64463c28b1cf329a524
Ankit Singh <ankit6aprsingh@gmail.com>
2023-10-02 22:10:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Get a reference to the div element with the ID "div1"
+var divElement = document.getElementById("div1");
+
+// Change the text color of the div to blue
+divElement.style.color = "blue";
+
+// Change the background color of the div to yellow
+divElement.style.backgroundColor = "yellow";
+
+// Add a margin of 20px to the div
+divElement.style.margin = "20px";
+
+// Add a padding of 10px to the div
+divElement.style.padding = "10px";
+
+// Change the font size of the text inside the div to 18px
+divElement.style.fontSize = "18px";
+
+// Set the font weight of the text inside the div to bold
+divElement.style.fontWeight = "bold";
+
+// Change the height of the div to 200px
+divElement.style.height = "200px";
+
+// Change the width of the div to 300px
+divElement.style.width = "300px";
